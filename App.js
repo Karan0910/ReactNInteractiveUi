be55ff940ce6bab2b8cd30e5b95d50e0e45c34a3
Karan Sharma <95sharma.karan@gmail.com>
2020-04-06 21:55:39 -0400

    Added layout animation

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput,Alert,Animated,Image} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput,LayoutAnimation,Animated,Image} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -8,12 +8,11 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   constuctor(props)
   {
     //super(props);
-    this.state={emailValid:false};
+    this.state={emailValid:false, rightPosition:100, inputShadow : 0.1};
   }
 
   state = {
@@ -36,7 +35,45 @@ export default class App extends Component<Props> {
     }).start();
   };
 
+  _onFocus = () => {
+    this.setState({inputShadow:0.90})
+      
+  };
+
   render() {
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#6e40a2',
+        padding:10
+      },
+      welcome: {
+        fontSize: 20,
+        textAlign: 'left',
+        margin: 10,
+        color:'#FFF',
+        alignSelf:'stretch',
+      },
+      borderStyle: {
+        backgroundColor: '#FFF',
+        borderRadius: 10,
+        backgroundColor: '#000',
+        elevation: 100,
+        shadowColor: 'black',
+        shadowOpacity: this.state.inputShadow,
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 10,
+      },
+      arrowStyle: { 
+        height:25,
+        width:25,
+        position : 'absolute' ,
+        right : this.state.rightPosition, 
+        top : 8 }
+    });
     return (
       <View style={styles.container}>
 
@@ -44,9 +81,8 @@ export default class App extends Component<Props> {
           <TextInput style={styles.welcome} onBlur={this._onBlur}
           onFocus={this._onFocus}
           onChangeText={(text) => { this.emailIsValid(text)}}></TextInput>
-          <Animated.View style={{ opacity :this.state.fadeIn,height:25,width:25, position : 'absolute' , right : 10, top : 8 }}>
-              <Image source={require('./src/images/arrow.png')}></Image>
-          </Animated.View>  
+          <Image style={styles.arrowStyle} source={require('./src/images/arrow.png')}></Image>
+           
           </View>
 
       </View>
@@ -58,18 +94,15 @@ export default class App extends Component<Props> {
     if(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email))
     {
       console.log(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email));
-      this._startIn();
-      this.setState({
-        fadeIn : new Animated.Value(1)
-        });
+      LayoutAnimation.spring();
+      this.setState({rightPosition:10})
     }
     else {
       console.log("hi");
+      LayoutAnimation.spring();
       
-      this._startOut();
-      this.setState({
-        fadeIn : new Animated.Value(0)
-        });
+      this.setState({rightPosition:-100})
+      
     }
     /*return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)*/
   }
@@ -78,24 +111,3 @@ export default class App extends Component<Props> {
 
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#6e40a2',
-    padding:10
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'left',
-    margin: 10,
-    color:'#FFF',
-    alignSelf:'stretch',
-  },
-  borderStyle: {
-    backgroundColor: '#FFF',
-    borderRadius: 10,
-    backgroundColor: '#000'
-  },
-});
